75d45c31e9c1550542fb5e939dd8ab802deb0676
spyeic <lderic0415@gmail.com>
2023-08-23 23:07:59 -0700

    finish q21

--- lesson_1_1/2/q21.py
+++ lesson_1_1/2/q21.py
@@ -1,12 +1,15 @@
 import turtle as trtl
 
 size = int(input("What size would you like the square to be? "))
-# size = 100
+front_color = input("What color would you like the front face to be? ")
+right_color = input("What color would you like the right face to be? ")
+up_color = input("What color would you like the up face to be? ")
+
 painter = trtl.Turtle()
 painter.pensize(3)
 
 # front face
-painter.fillcolor("blue")
+painter.fillcolor(front_color)
 painter.begin_fill()
 painter.goto(0, size)
 painter.goto(size, size)
@@ -15,7 +18,7 @@ painter.goto(0, 0)
 painter.end_fill()
 
 # right face
-painter.fillcolor("red")
+painter.fillcolor(right_color)
 painter.penup()
 painter.goto(size, size)
 painter.pendown()
@@ -27,7 +30,7 @@ painter.goto(size, size)
 painter.end_fill()
 
 # up face
-painter.fillcolor("yellow")
+painter.fillcolor(up_color)
 painter.penup()
 painter.goto(0, size)
 painter.pendown()
@@ -38,13 +41,26 @@ painter.goto(size, size)
 painter.goto(0, size)
 painter.end_fill()
 
-# TODO
 for i in range(1, 3):
     painter.penup()
     painter.goto(0, size / 3 * i)
     painter.pendown()
     painter.goto(size, size / 3 * i)
-    painter.goto(size * 1.3, size * 1.3 / 3 * i)
+    painter.goto(size * 1.3, size / 3 * i + size * 0.3)
+
+for i in range(1, 3):
+    painter.penup()
+    painter.goto(size / 3 * i, 0)
+    painter.pendown()
+    painter.goto(size / 3 * i, size)
+    painter.goto(size / 3 * i + size * 0.3, size * 1.3)
+
+for i in range(1, 3):
+    painter.penup()
+    painter.goto(0.3 * size / 3 * i, size + 0.3 * size / 3 * i)
+    painter.pendown()
+    painter.goto(size + 0.3 * size / 3 * i, size + 0.3 * size / 3 * i)
+    painter.goto(size + 0.3 * size / 3 * i, 0.3 * size / 3 * i)
 
 wn = trtl.Screen()
 wn.mainloop()
