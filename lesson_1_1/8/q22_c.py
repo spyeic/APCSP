2a8a4226d29be04b56efed3c5f9bd555f5229ed8
spyeic <lderic0415@gmail.com>
2023-09-18 09:55:33 -0700

    activity 1.1.8

---
+++ lesson_1_1/8/q22_c.py
@@ -0,0 +1,62 @@
+
+# Move turtles horizontally and vertically across screen.
+# Stopping turtles when they collide.
+import turtle as trtl
+
+# create two empty lists of turtles, adding to them later
+horiz_turtles = []
+vert_turtles = []
+
+# use interesting shapes and colors
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
+
+tloc = 50
+for s in turtle_shapes:
+    # create horizontal turtle
+    ht = trtl.Turtle(shape=s)
+    # add horizontal turtle to horiz_turtles list
+    horiz_turtles.append(ht)
+    ht.penup()
+    new_color = horiz_colors.pop()
+    # change color of horizontal turtle
+    ht.fillcolor(new_color)
+    # set to initial location and heading
+    ht.goto(-350, tloc)
+    ht.setheading(0)
+    # do the same for vertical turtle
+    vt = trtl.Turtle(shape=s)
+    vert_turtles.append(vt)
+    vt.penup()
+    new_color = vert_colors.pop()
+    vt.fillcolor(new_color)
+    vt.goto(-tloc, 350)
+    vt.setheading(270)
+    # increase location for next turtle
+    tloc += 50
+
+horiz_copy = horiz_turtles.copy()
+vert_copy = vert_turtles.copy()
+for step in range(50):
+    print(step)
+    for ht in horiz_turtles:
+        for vt in vert_turtles:
+            ht.forward(10)
+            vt.forward(10)
+            x1 = ht.xcor()
+            y1 = ht.ycor()
+            x2 = vt.xcor()
+            y2 = vt.ycor()
+            if abs(x1 - x2) < 20 and abs(y1 - y2) < 20:
+                horiz_turtles.remove(ht)
+                vert_turtles.remove(vt)
+
+for ht in horiz_copy:
+    ht.fillcolor("black")
+
+for vt in vert_copy:
+    vt.fillcolor("black")
+
+wn = trtl.Screen()
+wn.mainloop()
