8a2e2430da7aeb951726944dc87248d55a3e3534
spyeic <lderic0415@gmail.com>
2023-10-02 17:35:07 -0700

    add random stars in the background

--- lesson_1_1/9/project.py
+++ lesson_1_1/9/project.py
@@ -1,5 +1,6 @@
 import turtle as trtl
 import math
+import random
 
 is_show_orbit = input("Show orbit? (y/n): ")
 circle_count = float(input("How many circles? (1-10/inf): "))
@@ -11,24 +12,51 @@ wn.setup(width=900, height=900)
 # init background color
 wn.bgcolor("black")
 # register shapes
-wn.register_shape("earth.gif")
-wn.register_shape("sun.gif")
-wn.register_shape("moon.gif")
+shapes = ["earth", "sun", "moon", "star"]
+for shape in shapes:
+    wn.addshape(f"{shape}.gif")
+
+
+def new_element(name):
+    return trtl.Turtle(shape=f"{name}.gif", visible=False)
 
 
 # init elements
-sun = trtl.Turtle(shape="sun.gif")
-earth = trtl.Turtle(shape="earth.gif", visible=False)
+stars = []
+for i in range(5):
+    star = new_element("star")
+    star.penup()
+    star.pencolor("white")
+    star.speed(0)
+    if i % 2 == 0:
+        star.goto(500, random.randint(-450, 450))
+        star.setheading(random.randint(90, 270))
+    else:
+        star.goto(random.randint(-450, 450), 500)
+        star.setheading(random.randint(180, 360))
+    stars.append(star)
+
+sun = new_element("sun")
+
+earth = new_element("earth")
 earth.penup()
 earth.pencolor("red")
 earth.goto(300, 0)
-earth.showturtle()
+earth.speed(0)
 earth.setheading(90)
-moon = trtl.Turtle(shape="moon.gif", visible=False)
+
+moon = new_element("moon")
 moon.penup()
 moon.pencolor("white")
 moon.goto(380, 0)
+moon.speed(0)
+
+# show elements
+sun.showturtle()
+earth.showturtle()
 moon.showturtle()
+for star in stars:
+    star.showturtle()
 
 # draw orbit if needed
 if is_show_orbit == "y":
@@ -53,6 +81,20 @@ while circle_count > 0:
             earth.xcor() + moon_orbit_redius * math.cos(math.radians(day * 12)),
             earth.ycor() + moon_orbit_redius * math.sin(math.radians(day * 12)),
         )
+        # move stars for a step
+        for star in stars:
+            # if star is out of screen, move it to the other side
+            if star.xcor() > 500 or star.xcor() < -500 or star.ycor() > 500 or star.ycor() < -500:
+                star.hideturtle()
+                if round(random.random()) == 1:
+                    star.goto(500, random.randint(-450, 450))
+                    star.setheading(random.randint(90, 270))
+                else:
+                    star.goto(random.randint(-450, 450), 500)
+                    star.setheading(random.randint(180, 360))
+                star.showturtle()
+            # move star a random step
+            star.forward(random.randint(20, 40))
     circle_count -= 1
 
 wn.mainloop()
