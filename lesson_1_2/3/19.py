7904afefd39261d1816bf0773dba19071a78b85b
Eric <lderic0415@gmail.com>
2023-10-24 01:26:25 -0700

    fix error in activity 1.2.3

--- lesson_1_2/3/19.py
+++ lesson_1_2/3/19.py
@@ -14,5 +14,7 @@ for i in range(6):
 
 for active_turtle in turtle_list:
     active_turtle.forward(400)
+    active_turtle.setheading(active_turtle.heading()+180)
+    active_turtle.forward(250)
 
 trtl.mainloop()
