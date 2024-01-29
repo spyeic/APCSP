9dede524eb0a5df003ecc131e9f91ee1f33da77b
Eric <lderic0415@gmail.com>
2024-01-29 13:24:11 -0800

    new layout

--- lesson_2/2/7/project.py
+++ lesson_2/2/7/project.py
@@ -1,6 +1,3 @@
-# p227_starter_one_button_shell.py
-# Note this will not run in the code editor and must be downloaded
-
 import subprocess
 import tkinter as tk
 import tkinter.scrolledtext as tksc
@@ -111,22 +108,19 @@ def save(content=None):
 root = tk.Tk()
 root.title("GUI")
 frame = tk.Frame(root)
-frame.pack()
+frame.grid(row=0, column=0)
+controls_frame = tk.Frame(frame)
+controls_frame.grid(row=0, column=0)
 
 
 def create_function_button(text, command, args=None):
-    btn = tk.Button(frame, text=text, command=lambda: do_command_with_thread(command, args))
-    btn.pack()
+    return tk.Button(controls_frame, width=30, text=text, command=lambda: do_command_with_thread(command, args))
 
 
 def create_url_entry():
-    # creates the frame with label for the text box
-    frame_url = tk.Frame(frame, pady=10)  # change frame color
-    frame_url.pack()
-
     # decorative label
     url_label = tk.Label(
-        frame_url,
+        controls_frame,
         text="Enter a URL of interest: ",
         compound="center",
         font=("comic sans", 14),
@@ -135,9 +129,9 @@ def create_url_entry():
         cursor="heart",
         fg="red"
     )
-    url_label.pack(side=tk.LEFT)
-    u_entry = tk.Entry(frame_url, font=("comic sans", 14))  # change font
-    u_entry.pack(side=tk.LEFT)
+    url_label.grid(row=0, column=0)
+    u_entry = tk.Entry(controls_frame, font=("comic sans", 14))  # change font
+    u_entry.grid(row=1, column=0)
     return u_entry
 
 
@@ -146,45 +140,48 @@ def create_command_textbox():
     c_textbox = tksc.ScrolledText(frame)
     c_textbox.tag_config("red", foreground="red")
     c_textbox.tag_config("green", foreground="green")
-    c_textbox.pack()
-    save_btn = tk.Button(frame, text="Save", command=save)
-    save_btn.pack()
+    c_textbox.grid(row=0, column=1)
     return c_textbox
 
 
 def create_history_frame():
     h_frame = tk.Frame(root)
     h_text = tksc.ScrolledText(h_frame)
-    h_text.pack()
-
+    h_text.grid(row=0, column=0)
     history_save_btn = tk.Button(
         h_frame,
         text="Save",
         command=lambda: save("====================\n".join(history)),
     )
-    history_save_btn.pack()
+    history_save_btn.grid(row=1, column=0)
     return h_frame, h_text
 
 
 url_entry = create_url_entry()
-create_function_button("Check to see if a URL is up and active", "ping", ping_args)
-create_function_button("Get the IP address of a URL", "nslookup")
-create_function_button("Get the path of a URL", tracert_cmd, tracert_args)
-# Adds an output box to GUI.
+ping_btn = create_function_button("Check to see if a URL is up and active", "ping", ping_args)
+ping_btn.grid(row=2, column=0)
+nslookup_btn = create_function_button("Get the IP address of a URL", "nslookup")
+nslookup_btn.grid(row=3, column=0)
+tracert_btn = create_function_button("Get the path of a URL", tracert_cmd, tracert_args)
+tracert_btn.grid(row=4, column=0)
+save_and_history_frame = tk.Frame(controls_frame)
+save_and_history_frame.grid(row=5, column=0)
+save_btn = tk.Button(save_and_history_frame, text="Save", command=save)
+save_btn.grid(row=0, column=0)
+
 command_textbox = create_command_textbox()
 history_frame, history_text = create_history_frame()
 
-
 def show_main():
-    history_frame.pack_forget()
-    frame.pack()
+    history_frame.grid_forget()
+    frame.grid(row=0, column=0)
 
 
 def show_history():
     if check_is_running():
         return
-    frame.pack_forget()
-    history_frame.pack()
+    frame.grid_forget()
+    history_frame.grid(row=0, column=0)
     history_text.delete(1.0, tk.END)
     for item in history:
         history_text.insert(tk.END, item)
@@ -192,9 +189,9 @@ def show_history():
 
 
 back_btn = tk.Button(history_frame, text="Back", command=show_main)
-back_btn.pack()
+back_btn.grid(row=2, column=0)
 
-history_btn = tk.Button(frame, text="History", command=show_history)
-history_btn.pack()
+history_btn = tk.Button(save_and_history_frame, text="History", command=show_history)
+history_btn.grid(row=0, column=1)
 
 root.mainloop()
