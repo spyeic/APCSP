bcf5d77f78e97d7a1fa50b34c464f9b6dab0952e
Eric <lderic0415@gmail.com>
2024-01-29 09:31:45 -0800

    use functions

--- lesson_2/2/7/project.py
+++ lesson_2/2/7/project.py
@@ -18,8 +18,6 @@ def get_url():
     return url_val
 
 
-# Modify the do_command function:
-#   to use the new button as needed
 def do_command(command, args=None):
     global command_textbox, url_entry
     url_val = get_url()
@@ -31,9 +29,7 @@ def do_command(command, args=None):
     args = [command, url_val] + (args or [])
     print("Running command: ", args)
 
-    p = subprocess.Popen(
-        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )  # v2
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # v2
     while True:
         out = p.stdout.readline()
         if out == b"" and p.poll() is not None:
@@ -46,9 +42,6 @@ def do_command(command, args=None):
     prev = command_textbox.get("1.0", tk.END)
     history.append(prev)
     print("Done")
-    # cmd_results, cmd_errors = p.communicate()
-    # command_textbox.insert(tk.END, cmd_results)
-    # command_textbox.insert(tk.END, cmd_errors)
 
 
 # Save function.
@@ -88,47 +81,67 @@ else:
     root.title("Linux")
     ping_args.append("-c")
 ping_args.append(str(ping_times))
-# set up button to run the do_command function
-ping_btn = tk.Button(frame, text="Check to see if a URL is up and active",
-                     command=lambda: do_command("ping", ping_args))
-ping_btn.pack()
 
-nslookup_btn = tk.Button(frame, text="nslookup", command=lambda: do_command("nslookup"))
-nslookup_btn.pack()
 
-# creates the frame with label for the text box
-frame_URL = tk.Frame(frame, pady=10)  # change frame color
-frame_URL.pack()
+def create_function_button(text, command, args=None):
+    btn = tk.Button(frame, text=text, command=lambda: do_command(command, args))
+    btn.pack()
 
-# decorative label
-url_label = tk.Label(
-    frame_URL,
-    text="Enter a URL of interest: ",
-    compound="center",
-    font=("comic sans", 14),
-    bd=0,
-    relief=tk.FLAT,
-    cursor="heart",
-    fg="mediumpurple3"
-)
-url_label.pack(side=tk.LEFT)
-url_entry = tk.Entry(frame_URL, font=("comic sans", 14))  # change font
-url_entry.pack(side=tk.LEFT)
 
+def create_url_entry():
+    # creates the frame with label for the text box
+    frame_URL = tk.Frame(frame, pady=10)  # change frame color
+    frame_URL.pack()
+
+    # decorative label
+    url_label = tk.Label(
+        frame_URL,
+        text="Enter a URL of interest: ",
+        compound="center",
+        font=("comic sans", 14),
+        bd=0,
+        relief=tk.FLAT,
+        cursor="heart",
+        fg="mediumpurple3",
+    )
+    url_label.pack(side=tk.LEFT)
+    global url_entry
+    url_entry = tk.Entry(frame_URL, font=("comic sans", 14))  # change font
+    url_entry.pack(side=tk.LEFT)
+    return url_entry
+
+
+def create_command_textbox():
+    global command_textbox
+    # Adds an output box to GUI.
+    command_textbox = tksc.ScrolledText(frame)
+    command_textbox.pack()
+    save_btn = tk.Button(frame, text="Save", command=save)
+    save_btn.pack()
+
+
+def create_history_frame():
+    global history_frame, history_text
+    history_frame = tk.Frame(root)
+    history_text = tksc.ScrolledText(history_frame)
+    history_text.pack()
+
+    history_save_btn = tk.Button(
+        history_frame,
+        text="Save",
+        command=lambda: save("====================\n".join(history)),
+    )
+    history_save_btn.pack()
+
+
+create_url_entry()
+create_function_button("Check to see if a URL is up and active", "ping", ping_args)
+create_function_button("Get the IP address of a URL", "nslookup")
+create_function_button("Get the path of a URL", "traceroute")
 # Adds an output box to GUI.
-command_textbox = tksc.ScrolledText(frame)
-command_textbox.pack()
+create_command_textbox()
+create_history_frame()
 
-save_btn = tk.Button(frame, text="Save", command=save)
-save_btn.pack()
-
-history_frame = tk.Frame(root)
-
-history_text = tksc.ScrolledText(history_frame)
-history_text.pack()
-
-history_save_btn = tk.Button(history_frame, text="Save", command=lambda: save("====================\n".join(history)))
-history_save_btn.pack()
 
 def show_main():
     history_frame.pack_forget()
@@ -144,7 +157,7 @@ def show_history():
         history_text.insert(tk.END, "====================\n\n")
 
 
-back_btn = tk.Button(history_frame, text="Back", command=lambda: show_main())
+back_btn = tk.Button(history_frame, text="Back", command=show_main)
 back_btn.pack()
 
 history_btn = tk.Button(frame, text="History", command=show_history)
