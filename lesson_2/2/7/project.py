e32002280c842f7d2dd4d709989ad8dac3c28026
Eric <lderic0415@gmail.com>
2024-01-30 22:37:35 -0800

    activity 2.2.7

--- lesson_2/2/7/project.py
+++ lesson_2/2/7/project.py
@@ -10,7 +10,7 @@ import threading
 history = []
 is_running = False
 is_killed = False
-current_process = None
+current_process: subprocess.Popen | None = None
 
 ping_args = []
 ping_times = 5
@@ -35,7 +35,7 @@ def get_url():
 def check_is_running():
     global is_running
     if is_running:
-        showinfo("Error", "A command is already running")
+        showinfo("Error", "A command is already running, please wait or kill the command")
         return True
     return False
 
@@ -73,13 +73,14 @@ def do_command(command, args=None):
     return_code = p.poll()
     if is_killed:
         is_killed = False
-        print("Error: ", "{command} was killed")
+        print("Error: ", f"{command} was killed")
         command_textbox.insert(tk.END, f"\nprocess killed", "red")
     elif return_code != 0:
         err = p.stderr.readlines()
         err_str = b"\n".join(err).decode("utf-8")
         print("Error: ", err_str)
-        command_textbox.insert(tk.END, f"an error occurred during {command}:\n{err_str} with return code {return_code}", "red")
+        command_textbox.insert(tk.END, f"an error occurred during {command}:\n{err_str} with return code {return_code}",
+                               "red")
     else:
         command_textbox.insert(tk.END, f"{command} finished", "green")
         command_textbox.see(tk.END)
@@ -112,7 +113,7 @@ def save(content=None):
             ("All files", "*.*"),
         ),
     )
-    if filename is None:
+    if filename is None or filename == "":
         return
     file = open(filename, mode="w")
     if not content:
@@ -121,19 +122,29 @@ def save(content=None):
     file.write(content)
     file.close()
 
+
+fg_color = "sienna"
 bg_color = "cornsilk"
 font = ("Consolas", 12)
 
 root = tk.Tk()
 root.title("GUI")
-frame = tk.Frame(root, bg=bg_color)
+frame = tk.Frame(root, bg=bg_color, bd=10, relief=tk.RIDGE)
 frame.grid(row=0, column=0)
-controls_frame = tk.Frame(frame, bg=bg_color)
+controls_frame = tk.Frame(frame, bg=bg_color, pady=5, padx=5)
 controls_frame.grid(row=0, column=0)
 
 
 def create_function_button(text, command, args=None):
-    return tk.Button(controls_frame, width=30, text=text, command=lambda: do_command_with_thread(command, args))
+    return tk.Button(
+        controls_frame,
+        fg=fg_color,
+        bg="antique white",
+        width=33,
+        text=text,
+        command=lambda: do_command_with_thread(command, args),
+        borderwidth=1,
+    )
 
 
 def create_url_entry():
@@ -144,14 +155,13 @@ def create_url_entry():
         compound="center",
         font=font,
         bd=0,
-        relief=tk.FLAT,
         cursor="heart",
-        fg="red",
+        fg=fg_color,
         bg=bg_color
     )
-    url_label.grid(row=0, column=0)
-    u_entry = tk.Entry(controls_frame, font=("", 14))  # change font
-    u_entry.grid(row=1, column=0)
+    url_label.grid(row=1, column=0)
+    u_entry = tk.Entry(controls_frame, width=21, font=("", 14), cursor="tcross")  # change font
+    u_entry.grid(row=2, column=0)
     return u_entry
 
 
@@ -165,11 +175,13 @@ def create_command_textbox():
 
 
 def create_history_frame():
-    h_frame = tk.Frame(root)
-    h_text = tksc.ScrolledText(h_frame)
+    h_frame = tk.Frame(root, bg=bg_color, bd=10, relief=tk.RIDGE)
+    h_text = tksc.ScrolledText(h_frame, font=font)
     h_text.grid(row=0, column=0)
     history_save_btn = tk.Button(
         h_frame,
+        bg="antique white",
+        fg=fg_color,
         text="Save",
         command=lambda: save("====================\n".join(history)),
     )
@@ -177,22 +189,27 @@ def create_history_frame():
     return h_frame, h_text
 
 
+text_frame = tk.Frame(controls_frame, bg=bg_color)
+text_frame.grid(row=0, column=0)
+title = tk.Label(text_frame, text="IP Tools", font=(font[0], 20), fg="sienna", height=5, bg=bg_color)
+title.grid(row=0, column=0)
 url_entry = create_url_entry()
 ping_btn = create_function_button("Check to see if a URL is up and active", "ping", ping_args)
-ping_btn.grid(row=2, column=0)
+ping_btn.grid(row=3, column=0)
 nslookup_btn = create_function_button("Get the IP address of a URL", "nslookup")
-nslookup_btn.grid(row=3, column=0)
+nslookup_btn.grid(row=4, column=0)
 tracert_btn = create_function_button("Get the path of a URL", tracert_cmd, tracert_args)
-tracert_btn.grid(row=4, column=0)
+tracert_btn.grid(row=5, column=0)
 save_and_history_frame = tk.Frame(controls_frame)
-save_and_history_frame.grid(row=5, column=0)
-save_btn = tk.Button(save_and_history_frame, text="Save", command=save)
+save_and_history_frame.grid(row=6, column=0)
+save_btn = tk.Button(save_and_history_frame, bg="antique white", fg=fg_color, width=10, text="Save", command=save)
 save_btn.grid(row=0, column=0)
-kill_btn = tk.Button(save_and_history_frame, text="Kill", command=kill)
+kill_btn = tk.Button(save_and_history_frame, bg="antique white", fg=fg_color, width=10, text="Kill", command=kill)
 kill_btn.grid(row=0, column=2)
 
 command_textbox = create_command_textbox()
 history_frame, history_text = create_history_frame()
+
 
 def show_main():
     history_frame.grid_forget()
@@ -210,10 +227,11 @@ def show_history():
         history_text.insert(tk.END, "\n====================\n")
 
 
-back_btn = tk.Button(history_frame, text="Back", command=show_main)
+back_btn = tk.Button(history_frame, text="Back", bg="antique white", fg=fg_color, command=show_main)
 back_btn.grid(row=2, column=0)
 
-history_btn = tk.Button(save_and_history_frame, text="History", command=show_history)
+history_btn = tk.Button(save_and_history_frame, bg="antique white", fg=fg_color, width=10, text="History",
+                        command=show_history)
 history_btn.grid(row=0, column=1)
 
 root.mainloop()
